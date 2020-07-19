79e934457e4e4fd2dafd0d96bf1fb8bfe89b7566
gagan madan <gagan7535@gmail.com>
2020-07-19 20:48:41 +0530

    first part complte

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,17 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({children}) => {
+    const [ githubUser, setGithubUser ] = useState(mockUser);
+    const [ githubRepos, setGithubRepos ] = useState(mockRepos);
+    const [ githubFollowers, setGithubFollowers ] = useState(mockFollowers);
+
+    return <GithubContext.Provider value={{githubUser, githubRepos, githubFollowers}}>
+        {children}
+    </GithubContext.Provider>
+}
+
+export {GithubContext, GithubProvider}
